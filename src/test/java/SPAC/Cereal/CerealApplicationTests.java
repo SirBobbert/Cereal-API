ddb42626ab48439eb6c73cd49ba19521f2ce3ca5
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-09-29 13:18:30 +0200

    Initial commit

---
+++ src/test/java/SPAC/Cereal/CerealApplicationTests.java
@@ -0,0 +1,13 @@
+package SPAC.Cereal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
