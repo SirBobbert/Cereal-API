2a0893208ac067e5e16671a90ae4e563a7a54a8b
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-09-29 14:35:23 +0200

    update POM

--- src/main/java/SPAC/Cereal/CerealApplication.java
+++ src/main/java/SPAC/Cereal/CerealApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CerealApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CerealApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CerealApplication.class, args);
+    }
 }
